4b6f98ef3d69c45379d16a0bcbe9802b99fa4a58
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-02-09 12:51:46 +0000

    update Salary system question

--- Day_01_Class_Object/Example01/BaiscMain.java
+++ Day_01_Class_Object/Example01/BaiscMain.java
@@ -8,6 +8,16 @@ class BasicMain{
 
         // ClassName ObjectName = new ClassName()
 
+        Employee emp = new Employee();
+        emp.get_info();
+        emp.get_salary(56000, 7);
+        emp.get_salary(56000, 3);
+
+        System.out.println("Your City Name is : "+emp.get_city());
+        System.out.println("Your height Name is : "+emp.get_height());
+        System.out.println("Your price Name is : "+emp.get_price());
+        System.out.println("this is result : "+emp.get_result(700,800));
+
     }
 
 }
--- Day_01_Class_Object/Example01/Employee.java
+++ Day_01_Class_Object/Example01/Employee.java
@@ -8,7 +8,45 @@ public class Employee {
         System.out.println("Employee Name : joy");
     }
     // without args and with return type.
+
+    int get_price(){
+
+        return 999;
+    }
+
+    String get_city(){
+
+        return "mumbai";
+    }
+
+    float get_height(){
+
+        return 5.6f;
+    }
+
     // without return type and with args.
+
+    void get_salary(int salary,int leave){
+        int oneDaySalary = salary/30;
+        int leave_amount = oneDaySalary*leave;
+        int final_salary = salary - leave_amount;
+
+        System.out.println("---------Salary---------------");
+        System.out.println("Monthly Salary : "+salary);
+        System.out.println("leave : "+leave);
+        System.out.println("leave amount : "+leave_amount);
+        System.out.println("Pay Salary : "+final_salary);
+        System.out.println("------------------------");
+
+    }
+
     // with return type and with args.
+
+    int get_result(int number1,int number2){
+
+        int result = number1 + number2;
+        
+        return result;
+    }
 
 }
