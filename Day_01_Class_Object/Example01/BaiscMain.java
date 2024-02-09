b7987e2bcda73641c0111ba5984610eb891d54dd
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-02-09 12:31:49 +0000

    update employee class

---
+++ Day_01_Class_Object/Example01/BaiscMain.java
@@ -0,0 +1,13 @@
+class BasicMain{
+
+    public static void main(String[] args){
+
+        // System.out.println("codeswithpankaj");
+
+        // create an object : 
+
+        // ClassName ObjectName = new ClassName()
+
+    }
+
+}
---
+++ Day_01_Class_Object/Example01/Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+    
+    
+
+
+    // basic = without args. and without return type.
+    void get_info(){
+        System.out.println("Employee Name : joy");
+    }
+    // without args and with return type.
+    // without return type and with args.
+    // with return type and with args.
+
+}
