51bb0476ea4b6095301442ff189f5b82c58607a1
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-08-01 12:41:38 +0000

    update this keyword

---
+++ Day_10/Employees.java
@@ -0,0 +1,34 @@
+package Day_10;
+
+class Employee{
+
+    String emp_name; 
+    double salary;
+
+    Employee(String emp_name,double salary){
+
+        emp_name = emp_name;
+        salary = salary;
+
+    }
+
+    String get_name(){
+        return emp_name;
+    }
+
+    double get_salary(){
+        return salary;
+    }
+
+    void set_name(String emp_name){
+        emp_name = emp_name;
+    }
+
+    void set_salary(double salary){
+        salary = salary;
+    }
+
+    
+
+
+}
