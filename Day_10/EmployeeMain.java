4fed552174ec482b2720cfeb9d8d3d45da14abee
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-08-01 13:32:04 +0000

    update files this keyword

--- Day_10/EmployeeMain.java
+++ Day_10/EmployeeMain.java
@@ -6,7 +6,12 @@ class EmployeeMain{
     public static void main(String[] args) {
      
         Employees emps = new Employees("Joy",45000.89);
+        Employees emp1 = new Employees("Nishant",65000.89);
         System.out.println(emps.get_name());
         System.out.println(emps.get_salary());
+        
+        System.out.println(emp1.get_name());
+        System.out.println(emp1.get_salary());
+
     }
 }
