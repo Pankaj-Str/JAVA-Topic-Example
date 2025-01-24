df2bae74653ffe02e6c6b6bc4dd130d7d9605592
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-01-24 13:53:57 +0530

    add Function_01 with info method and main execution

---
+++ Day_12/Function_01.java
@@ -0,0 +1,31 @@
+package Day_12;
+
+public class Function_01 {
+
+    // Function Definition
+
+    // void functionName(){
+         // Function Body
+    // }
+
+    // create a function name of info
+
+    static void info(){
+        System.out.println("welcome to codes with pankaj");
+    }
+
+
+    public static void main(String[] args) {
+        
+
+        // Function Calling
+        info();
+    }
+
+
+
+
+
+
+    
+}
