86e84e45d14fe30226b90abae20af813489b33c9
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-01-24 13:55:28 +0530

    add FunctionWithArg class with initial documentation

---
+++ Day_12/FunctionWithArg.java
@@ -0,0 +1,13 @@
+package Day_12;
+
+public class FunctionWithArg {
+
+
+    // what is function with argument
+    // function with argument is a function that takes some input and perform some operation and return some output
+    // function with argument is also known as function with parameter
+
+    
+
+    
+}
