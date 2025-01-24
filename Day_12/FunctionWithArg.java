99aefbfcb0b9210a626e4665905ea67c3afcac5a
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-01-24 13:59:38 +0530

    add add method to FunctionWithArg class and demonstrate usage in main

--- Day_12/FunctionWithArg.java
+++ Day_12/FunctionWithArg.java
@@ -7,7 +7,20 @@ public class FunctionWithArg {
     // function with argument is a function that takes some input and perform some operation and return some output
     // function with argument is also known as function with parameter
 
-    
+
+    // create a function with argument
+
+    static void add(int a, int b){
+        int sum = a + b;
+        System.out.println("Sum of a and b is: "+sum);
+    }
+
+    public static void main(String[] args) {
+        
+        // calling function with argument
+        add(10, 20);
+
+    }
 
     
 }
