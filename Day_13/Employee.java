b36a16cede13a48942c3c890ea1e2f72c7297036
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-01-24 14:08:07 +0530

    add Employee class with info method and main execution

---
+++ Day_13/Employee.java
@@ -0,0 +1,27 @@
+package Day_13;
+
+public class Employee {
+
+    // create a method 
+
+    void info(){
+        System.out.println("Employee Information");
+        System.out.println("Employee Name: Pankaj");
+        System.out.println("Employee Age: 25");
+        System.out.println("Employee Salary: 50000");
+    }   
+
+
+    public static void main(String[] args) {
+        
+        // create object
+
+        // class_name object_name = new class_name();
+
+        Employee emp = new Employee();
+        emp.info();
+
+
+    }
+    
+}
