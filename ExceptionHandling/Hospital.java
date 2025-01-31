c9eaf1a32096c518370f5a1e27924e2328c536aa
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-01-31 13:41:10 +0530

    add Hospital example demonstrating BMI calculation with exception handling

---
+++ ExceptionHandling/Hospital.java
@@ -0,0 +1,33 @@
+package ExceptionHandling;
+import java.util.Scanner;
+public class Hospital {
+    
+    public static void main(String[] args) {
+        
+
+        // create a BMI calculator 
+        try{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Enter your weight in kg: \n");
+        int weight = sc.nextInt();
+        System.out.println("\n Enter your height in meters: \n");
+        int height = sc.nextInt();
+        double bmi = weight / (height * height);
+
+        if(bmi < 18.5){
+            throw new ArithmeticException("Underweight");
+        } else if(bmi >= 18.5 && bmi < 24.9){
+            System.out.println("Normal weight");
+        } else if(bmi >= 25 && bmi < 29.9){
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obesity");
+        }
+    } catch(ArithmeticException e){
+        System.out.println("Do not Enter 0 for height");
+
+
+    }
+    
+}
+}
