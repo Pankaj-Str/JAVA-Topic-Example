c87afed146845a00c936cfa85e9d0451fbc25905
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-02-06 07:54:18 +0530

    add example demonstrating usage of StringBuilder and StringBuffer in Java

---
+++ Java_String/Advance_Example.java
@@ -0,0 +1,17 @@
+package Java_String;
+
+public class Advance_Example {
+
+    public static void main(String[] args) {
+        
+        String str = "Hello, World!";
+
+        StringBuilder obj = new StringBuilder(str);
+        System.out.println(obj);
+
+        StringBuffer data1 = new StringBuffer("welcome");
+        System.out.println(data1);
+
+    }
+    
+}
